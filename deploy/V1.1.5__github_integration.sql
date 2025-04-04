3ad1f3a624589dc6fdb63ec78b10603d501c6109
avuthusiva <asvr410@gmail.com>
2025-04-04 12:22:34 +0530

    Added repositories into the file

--- deploy/V1.1.5__github_integration.sql
+++ deploy/V1.1.5__github_integration.sql
@@ -4,3 +4,9 @@ create or replace api integration github_int
 enabled = True
 api_provider = git_https_api
 api_allowed_prefixes = ("https://github.com/avuthusiva/snowflake_03042025.git");
+
+create or replace git repository github_repo_prac
+api_integration = github_int
+origin = "https://github.com/avuthusiva/snowflake_03042025.git";
+
+alter git repository github_repo_prac fetch;
