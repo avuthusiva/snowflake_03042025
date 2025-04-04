7e28ef10ca7430feb04dea2fd0713b1eab8623d5
avuthusiva <asvr410@gmail.com>
2025-04-04 10:50:40 +0530

    added file formats

---
+++ deploy/V1.1.3__file_formats.sql
@@ -0,0 +1,18 @@
+use schema my_db.github_schema;
+
+create or replace file format csv_skip_header
+type = csv
+skip_header = 1
+field_optionally_enclosed_by = '\042';
+
+create or replace file format csv_parse_header
+type = csv
+parse_header = 1
+field_optionally_enclosed_by = '\042';
+
+create or replace file format json_format 
+type = json
+strip_outer_array = True;
+
+create or replace file format json_format 
+type = parquet;
