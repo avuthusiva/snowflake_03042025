d1c2f1f404cabe2f646a80b2a743c3c122c047d1
avuthusiva <asvr410@gmail.com>
2025-04-04 10:34:53 +0530

    alter table data

---
+++ deploy/V1.1.2_alter_user_data_table.sql
@@ -0,0 +1,5 @@
+use schema my_db.github_schema;
+
+alter table user_data add column status varchar(1);
+
+update user_data set status = 'A';
---
+++ scripts/script.sql
@@ -0,0 +1,11 @@
+use role accountadmin;
+
+use warehouse my_warehouse;
+
+use schema my_db.github_schema;
+
+show tables;
+
+select * from user_data;
+
+desc table user_data;
