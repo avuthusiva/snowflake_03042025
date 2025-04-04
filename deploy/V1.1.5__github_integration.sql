621d8a11711cad71acbf6d504ce69b6074fd1b48
avuthusiva <asvr410@gmail.com>
2025-04-04 12:01:46 +0530

    Added double quotes in the file

--- deploy/V1.1.5__github_integration.sql
+++ deploy/V1.1.5__github_integration.sql
@@ -3,8 +3,8 @@ use schema my_db.github_schema;
 create api integration if not exists github_schema.github_int
 enabled = True
 api_provider = api_https_git
-api_allowed_prefixes = ('https://github.com/avuthusiva/snowflake_03042025.git');
+api_allowed_prefixes = ("https://github.com/avuthusiva/snowflake_03042025.git");
 
 create git repository github_schema.github_repo
-api_integration = 'github_int'
-origin = 'https://github.com/avuthusiva/snowflake_03042025.git';
+api_integration = "github_int"
+origin = "https://github.com/avuthusiva/snowflake_03042025.git";
