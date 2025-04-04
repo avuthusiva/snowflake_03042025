3655e0b597d9d3ee3a96c5173a9c7b4eb9ff037b
avuthusiva <asvr410@gmail.com>
2025-04-04 12:08:28 +0530

    Added new file

--- deploy/V1.1.5__github_integration.sql
+++ deploy/V1.1.5__github_integration.sql
@@ -1,10 +1,10 @@
 use schema my_db.github_schema;
 
-create api integration if not exists github_schema.github_int
+create or replace api integration github_schema.github_int
 enabled = True
 api_provider = git_https_api
 api_allowed_prefixes = ("https://github.com/avuthusiva/snowflake_03042025.git");
 
-create git repository github_schema.github_repo
-api_integration = "github_int"
+create or replace git repository github_schema.github_repo
+api_integration = github_int
 origin = "https://github.com/avuthusiva/snowflake_03042025.git";
