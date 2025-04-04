7fd15c247806d6bb305022baa37bfbc9b95406ad
avuthusiva <asvr410@gmail.com>
2025-04-04 12:04:21 +0530

    Removed error in the file

--- deploy/V1.1.5__github_integration.sql
+++ deploy/V1.1.5__github_integration.sql
@@ -2,7 +2,7 @@ use schema my_db.github_schema;
 
 create api integration if not exists github_schema.github_int
 enabled = True
-api_provider = api_https_git
+api_provider = git_https_api
 api_allowed_prefixes = ("https://github.com/avuthusiva/snowflake_03042025.git");
 
 create git repository github_schema.github_repo
